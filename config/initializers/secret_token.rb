dbf9b8aac7fedf3f6febbd4b58f0ed11b2539227
Evan Korzon <evan@thelevelup.com>
2016-11-24 15:48:48 -0500

    New repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Arcademovelist::Application.config.secret_key_base = 'ca4ee2bf9d25e822c22363de868b0082eb2d8f654dc8e3a6d031f58fa6b880c60d5d6160cf0358ab4e3b753f6de952a232bc236c1b9f87a18582c2f817ecd9c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Arcademovelist::Application.config.session_store :cookie_store, key: '_arcademovelist_session'
